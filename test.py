bf3996a580b00e47e30ee8ec701613c2aeb5a608
ma-kath <makathosn@gmail.com>
2025-08-25 16:21:54 +0200

    Up to date repo order

--- test.py
+++
@@ -1,6 +0,0 @@
-import os
-from dotenv import load_dotenv
-# Load environment variables from .env file
-load_dotenv()
-
-print(os.getenv('DATABASE_NAME'))  # This should print the value of DATABASE_NAME
